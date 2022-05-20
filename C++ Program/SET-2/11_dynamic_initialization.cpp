a4c4ec391036f6b8ab49433db29ac9af88546e11
YASHRAJ-TANDEL-1 <105947495+YASHRAJ-TANDEL-1@users.noreply.github.com>
2022-05-21 01:35:32 +0530

    Add files via upload

---
+++ C++ Program/SET-2/11_dynamic_initialization.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    float radius;
+    cout<<"Enter the radius of circle =";
+    cin>>radius;
+    float area=3.14*radius*radius;
+    cout<<"area of circle="<<area;
+    return(0);
+}

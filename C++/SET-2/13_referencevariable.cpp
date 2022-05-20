ff05bfc0e117a22c69fb96e60c425ce7254e7486
YASHRAJ-TANDEL-1 <105947495+YASHRAJ-TANDEL-1@users.noreply.github.com>
2022-05-21 01:44:43 +0530

    Add files via upload

---
+++ C++/SET-2/13_referencevariable.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+    int a=100;
+    int &b=a;
+    cout<<"variable ="<<a;
+    cout<<endl<<"refrence varibale ="<<b;
+    return(0);
+}

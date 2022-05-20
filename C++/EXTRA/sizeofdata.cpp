7d47127cab2d40c287f0aa90b1bd2b7017e92bc3
YASHRAJ-TANDEL-1 <105947495+YASHRAJ-TANDEL-1@users.noreply.github.com>
2022-05-21 01:50:43 +0530

    Add files via upload

---
+++ C++/EXTRA/sizeofdata.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     int a;
+     char b;
+     float c;
+     double d;
+
+     
+     cout<<"Size of int is "<<sizeof(a)<<endl;
+     cout<<"Size of char is "<<sizeof(b)<<endl;
+     cout<<"Size of float is "<<sizeof(c)<<endl;
+     cout<<"Size of double is "<<sizeof(d)<<endl;
+     return 0;  
+}

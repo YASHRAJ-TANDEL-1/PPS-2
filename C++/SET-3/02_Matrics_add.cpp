e1eae6013d1732cdfd6d5f9ad806a0b22986585e
YESRAJ® <105947495+YASHRAJ-TANDEL-1@users.noreply.github.com>
2022-05-21 14:29:12 +0530

    Add files via upload

---
+++ C++/SET-3/02_Matrics_add.cpp
@@ -0,0 +1,48 @@
+/* WAP to add two matrices using default argument */
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+void AddMatrics(int r=2, int c=2)
+{
+    int a[r][c], b[r][c], i, j;
+
+    cout<<endl<<"Enter the values of First matrics"<<endl;
+    for( i=0; i<r; i++)
+    {
+        for( j=0; j<c; j++)
+        {
+             cin>> a[i][j];
+        }     
+    }
+
+    cout<<endl<<"Enter the values of Second matrics"<<endl;
+    for(i=0; i<r; i++)
+    {
+        for(j=0; j<c; j++)
+        {
+             cin>> b[i][j];
+        }     
+    }
+
+    cout<<endl<<"sumation of two matrics is "<<endl;
+    for(i=0; i<r; i++)
+    {
+        for(j=0; j<c; j++)
+        {
+             cout<<a[i][j]    +   b[i][j];
+        }     
+    }  
+}
+int main()
+{
+    int r,c;
+    cout<<"Enter the size of matrics you want ="<<endl;
+    cin>>r>>c;
+    cout<<"row="<<r <<endl<<"coloumn="<<c;
+
+    AddMatrics(r,c);
+    return 0;
+}      
+  
+  

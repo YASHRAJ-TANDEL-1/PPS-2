8c29a8c23d6b0df155e040ab1acd45d5df44da0c
YESRAJ® <105947495+YASHRAJ-TANDEL-1@users.noreply.github.com>
2022-05-23 10:04:39 +0530

    Add files via upload

---
+++ C++/SET-4/01_cricket_info.cpp
@@ -0,0 +1,58 @@
+/* Create a class player with the following data members
+name, age, runs, hi, lo, tsts, avg
+Write member functions for each of the following
+a. To get the data
+b. To display the data
+c. To calculate the average of the player */
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+class player
+{
+    private:
+            string name;
+            int age;
+            int run;
+            int HI,LO,test;
+            float avg;
+    public:
+            void getdata()
+            {
+                cout<<"DETAILS OF PLAYER"<<endl;
+                cout<<"Enter the Name of player"<<endl;
+                cin.ignore();
+                getline(cin,name);
+                cout<<"Enter the Age of player"<<endl;
+                cin>>age;
+                cout<<"Enter the Run of player"<<endl;
+                cin>>run;
+                cout<<"Enter the Highest Score of player"<<endl;
+                cin>>HI;
+                cout<<"Enter the Lowest Score of player"<<endl;
+                cin>>LO;
+                cout<<"Enter the Number of Test Match of player"<<endl;
+                cin>>test;
+            } 
+            void showdata()
+            {
+                cout<<setw(15)<<name<<setw(15)<<age<<setw(15)<<run<<setw(15)<<HI<<setw(15)<<LO<<setw(25)<<test<<setw(15)<<avg<<endl; 
+            }       
+};
+int main()
+{
+    int n;
+    cout<<"Enter the number of player deatail you want"<<endl;
+    cin>>n;
+    player p[n];
+    for(int i=0; i<n; i++)
+    {
+         p[i].getdata();
+    }
+    cout<<setw(15)<<"NAME"<<setw(15)<<"AGE"<<setw(15)<<"RUN"<<setw(15)<<"HIGEST SCORE"<<setw(15)<<"LOWEST SCORE"<<setw(25)<<"NUMBER OF TEST MATCH"<<setw(15)<<"AVERAGE"<<endl;
+    for(int i=0; i<n; i++)
+    { 
+      p[i].showdata();
+    }  
+    return 0;
+}

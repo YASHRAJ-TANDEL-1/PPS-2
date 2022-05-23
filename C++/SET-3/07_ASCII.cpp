76540db1e451843110cf7ca1c666bcaa6b19c8e6
YESRAJ® <105947495+YASHRAJ-TANDEL-1@users.noreply.github.com>
2022-05-23 10:08:31 +0530

    Add files via upload

---
+++ C++/SET-3/07_ASCII.cpp
@@ -0,0 +1,34 @@
+/*Write overloaded functions to convert ascii to int and ascii to
+float. */
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+void ascii(int a,char c)
+{
+    /*char c;
+    cout<<"Enter the character"<<endl;
+    cin>>c;*/
+    cout<<"ascii + integer ="<< c+a <<endl;
+}
+void ascii(float b,char c)
+{
+    cout<<"ascii+float="<<c+b;
+}
+
+
+int main()
+{
+    char c;
+    int a;
+    float b;
+    cout<<"Enter the character"<<endl;
+    cin>>c;
+    cout<<"Enter the integer"<<endl;
+    cin>>a;
+    cout<<"Enter the float"<<endl;
+    cin>>b;
+    ascii(a,c);
+    ascii(b,c);
+    return 0;
+}

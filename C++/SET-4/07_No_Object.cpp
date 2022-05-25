ff18b8e704593719a713298e69daf40ec1ac488f
YESRAJ® <105947495+YASHRAJ-TANDEL-1@users.noreply.github.com>
2022-05-25 22:44:23 +0530

    Add files via upload

---
+++ C++/SET-4/07_No_Object.cpp
@@ -0,0 +1,25 @@
+/* 7. Write a program to count number of objects created for any
+class.*/
+
+#include<iostream>
+using namespace std;
+class object
+{
+    public:
+    static int count;
+    object()
+    {
+        count++;
+    }
+};
+int object::count;
+int main()
+{
+    int n;
+    cout<<"Enter the number of data you want"<<endl;
+    cin>>n;
+    object c[n];
+    cout<<"The object created is "<<endl;
+    cout<<object::count;
+    return 0;
+}

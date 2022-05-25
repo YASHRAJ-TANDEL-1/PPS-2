7b1c39716d5b5f8dd5801493f1d1c4a0c024f24f
YESRAJ® <105947495+YASHRAJ-TANDEL-1@users.noreply.github.com>
2022-05-25 22:36:30 +0530

    Add files via upload

---
+++ C++/SET-4/06_4_time.cpp
@@ -0,0 +1,62 @@
+/* 4. Create a time with following data members
+int h,m,s
+Write member functions for each of the following
+a. To get the data in number of seconds
+b. To set the data in number of seconds
+c. To display the data
+d. To convert seconds into h, m, s 
+USING CONSTRUCTOR AND DESTRUCTOR*/
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+class time
+{
+    private:
+            int hour,min,sec,s;
+    public:
+            time()
+            {
+                cout<<"Constructer is called"<<endl;
+                hour=0;
+                min=0;
+                sec=0;
+                s=0;
+            }
+            void getdata()
+            {
+                cout<<"Enter the data in number of second =";
+                cin>>s;
+            }
+            void convert()
+            {
+                hour=(s/3600);
+                min=(s-(3600*hour))/60;
+                sec=(s-(3600*hour)-(min*60));
+            }
+            void showdata()
+            {
+                cout<<"TIME :"<<hour<<"hr :"<<min<<" min :"<<s<<"sec :"<<endl;
+            }  
+            ~time()
+            {
+                cout<<"Destructor is called"<<endl;
+            }     
+};
+int main()
+{
+    int n;
+    cout<<"Enter the number of data you want"<<endl;
+    cin>>n;
+    class time t[n];
+    for(int i=0; i<n; i++)
+    {
+        t[i].getdata();
+    }
+    for(int i=0; i<n; i++)
+    {
+        t[i].convert();
+        t[i].showdata();
+    }
+}

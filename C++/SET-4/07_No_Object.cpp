406c24c7222b1044d5a3ae47cd6d652716a5cf81
YESRAJ® <105947495+YASHRAJ-TANDEL-1@users.noreply.github.com>
2022-05-30 13:03:36 +0530

    Update 07_No_Object.cpp

--- C++/SET-4/07_No_Object.cpp
+++ C++/SET-4/07_No_Object.cpp
@@ -19,6 +19,7 @@ int main()
     cout<<"Enter the number of data you want"<<endl;
     cin>>n;
     object c[n];
+    cout<<endl;
     cout<<"Total object created is "<<endl;
     cout<<object::count;
     return 0;

b7f90778a66dd9a2b2f844144c9153077d552263
YESRAJ® <105947495+YASHRAJ-TANDEL-1@users.noreply.github.com>
2022-06-10 01:45:14 +0530

    Add files via upload

---
+++ C++/00_QUE_BANK_SOLUTION/M3_26_inline.cpp
@@ -0,0 +1,13 @@
+/* Q.26 Explain inline function with example. */
+
+#include <iostream>
+using namespace std;
+inline int cube(int s)
+{
+    return s*s*s;
+}
+int main()
+{
+    cout << "The cube of 3 is: " << cube(3) ;
+    return 0;
+}

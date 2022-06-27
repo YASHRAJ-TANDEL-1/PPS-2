f97180fa4773dc05b1d23448acb1690df362ce38
YESRAJ® <105947495+YASHRAJ-TANDEL-1@users.noreply.github.com>
2022-06-27 16:06:23 +0530

    Add files via upload

---
+++ C++/SET-6/08_2_RE_SAME.cpp
@@ -0,0 +1,144 @@
+/* 8. Rewrite assignment 2(from assignment 6) using +=,-=, *= &
+/=operator overloading. */
+
+#include<iostream>
+#include<iomanip>
+using namespace std;
+class matrix
+{
+    private: 
+            float m[5][5];
+    public:
+            matrix()
+            {
+                /* Constructer is called.*/
+                for(int i=0 ;i<3 ;i++)
+    	        {
+                    for(int j=0 ;j<3 ;j++)
+                    {
+			            m[i][j]=0;
+                    }
+		        }
+	        }
+            void getdata()
+            {
+                for(int i=0 ;i<3 ;i++)
+    	        {
+                    for(int j=0 ;j<3 ;j++)
+                    {
+			            cin>>m[i][j];
+                    }
+		        }
+            }
+            void showdata()
+            {
+                for(int i=0 ;i<3 ;i++)
+    	        {
+                    for(int j=0 ;j<3 ;j++)
+                    {
+			            cout<<m[i][j]<<" ";
+                    }
+                    cout<<endl;
+		        }
+            }
+            matrix operator +(matrix a)
+            {
+                matrix  b;
+                for(int i=0 ;i<3 ;i++)
+                {
+                    for(int j=0 ;j<3 ;j++)
+                    {
+                        b.m[i][j] = m[i][j] + a.m[i][j];
+                    }
+                    cout<<endl;
+                }
+                return b;
+            }
+            matrix operator -(matrix a)
+            {
+                matrix  b;
+                for(int i=0 ;i<3 ;i++)
+                {
+                    for(int j=0 ;j<3 ;j++)
+                    {
+                        b.m[i][j] = m[i][j] - a.m[i][j];
+                    }
+                    cout<<endl;
+                }
+                return b;
+            }
+            matrix operator *(matrix a)
+            {
+                matrix  b;
+                for(int i=0 ;i<3 ;i++)
+                {
+                    for(int j=0 ;j<3 ;j++)
+                    {
+                        for(int k=0 ;k<3 ;k++)
+                        {
+                            b.m[i][j] = m[i][j] - a.m[i][j];
+                        }
+                        cout<<endl;
+                    } 
+                }
+                return b;
+            }
+            matrix operator /(matrix a)
+            {
+                matrix  b;
+                for(int i=0 ;i<3 ;i++)
+                {
+                    for(int j=0 ;j<3 ;j++)
+                    {
+                        for(int k=0 ;k<3 ;k++)
+                        {
+                            b.m[i][j] = m[i][j] / a.m[i][j];
+                        }
+                        cout<<endl;
+                    } 
+                }
+                return b;
+            }
+            ~matrix()
+            {
+                /* Destructor is called.*/
+                for(int i=0 ;i<3 ;i++)
+    	        {
+                    for(int j=0 ;j<3 ;j++)
+                    {
+			            m[i][j]=0;
+                    }
+		        }
+            }
+};
+int main()
+{
+    matrix x,y,z;
+
+    cout<<"Enter value of first 3x3 matrix"<<endl;
+    x.getdata();
+    cout<<"Enter value of second 3x3 matrix"<<endl;
+    y.getdata();
+    cout<<endl;
+    cout<<"FIRST MATRIXS"<<endl;
+    x.showdata();
+    cout<<"SECOND MATRIXS"<<endl;
+    y.showdata();
+
+    z=x+y;
+    cout<<"ADDITION"<<endl;
+    z.showdata(); 
+
+    z=x-y;
+    cout<<"SUBTRACTION"<<endl;
+    z.showdata();
+
+    z=x*y;
+    cout<<"MULTIPLICATION"<<endl;
+    z.showdata();
+
+    z=x/y;
+    cout<<"DIVISION"<<endl;
+    z.showdata();
+return 0;
+}
